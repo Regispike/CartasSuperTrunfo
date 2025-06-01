47e046ba8712ee318cfd2b29580957b66c08c1ce
Regispike <202502952651@alunos.unifbv.edu.br>
2025-06-01 03:52:52 +0000

    Criei o código da segunda carta.

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -12,6 +12,14 @@ int main() {
     float pib = 75256.54;
     int pontos_turisticos = 5;
 
+    char carta2;
+    char estado2 = 'B';
+    char codigo2[4] = "B02";
+    int populacao2 = 3000000;
+    float area2 = 450.5;
+    float pib2 = 80356.59;
+    int pontos_turisticos2 = 10;
+
     printf("Carta1: %s\n", codigo);
     scanf("%s", &codigo);
 
@@ -27,6 +35,20 @@ int main() {
     printf("Número de pontos turisticos: %d\n", pontos_turisticos);
     scanf("%.2d", &pontos_turisticos);
 
+    printf("Carta2: %s\n", codigo2);
+    scanf("%s", &codigo2);
+
+    printf("População: %d\n", populacao2);
+    scanf("%d", &populacao2);
+
+    printf("Área: %.2f\n", area2);
+    scanf("%.2f", &area2);
+
+    printf("PIB: %.2f\n", pib2);
+    scanf("%f", &pib2);
+
+    printf("Número de pontos turisticos: %d\n", pontos_turisticos2);
+    scanf("%.2d", &pontos_turisticos2);
 
 
 return 0;
